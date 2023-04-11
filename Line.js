61a14a5bf80240f367d96c6d3cee6a9306d98dc3
Francois Nicholas Hentschel <the3non0_o@outlook.com>
2023-04-11 10:55:29 +0400

    Create Line.js

---
+++ Line.js
@@ -0,0 +1,40 @@
+// Retrieve data from file
+fetch('data.txt')
+  .then(response => response.text())
+  .then(data => {
+    const labels = [];  // Store x-axis labels
+    const values = [];  // Store y-axis values
+    
+    // Parse data into labels and values
+    const lines = data.split('\n');
+    lines.forEach(line => {
+      const [label, value] = line.split(':');
+      labels.push(label);
+      values.push(value);
+    });
+    
+    // Create chart using Chart.js
+    const chart = new Chart(document.getElementById('Linechart'), {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Data',
+          data: values,
+          backgroundColor: 'rgba(54, 162, 235, 0.5)'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error retrieving data:', error);
+  });
